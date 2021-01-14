12e0112d32419c12ba61ca13b286ae17842bb4fa
jahongirhusanov <jahongirhusanov@Jahongirs-MacBook-Pro.local>
2021-01-13 18:51:41 -0600

    v0.0.3 - Tour Components setUp

---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Tour() {
+  return <div>Hello</div>
+}
+
+export default Tour
---
+++ src/components/TourList/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tour from '../Tour'
+
+function TourList() {
+  return (
+    <div>
+      Hello from tourList
+      <Tour />
+    </div>
+  )
+}
+
+export default TourList
